97cc0a3a7dd5c5c847e0cfb194690848636d0068
harshnisar <harshnisar@gmail.com>
2021-03-26 11:11:15 +0530

    added the new setu[

--- download/setup.py
+++ download/setup.py
@@ -11,6 +11,7 @@ import json
 import zipfile
 import requests
 import py_compile
+import time
 
 task_summary = []
 
@@ -20,20 +21,27 @@ if system == 'posix':
 elif system == 'nt':
     bs = '\\'
 
+
+print('\nHello from harshnisar.com.\nWelcome to the setup process.\n'+'__'*40)
+time.sleep(1)
 #dependencies
-print('Checking dependencies...')
+
+print('\nChecking dependencies...')
+time.sleep(1)
 dependencies = ['kiteconnect','pandas','selenium','time','threading','urllib','requests','random','datetime','json','os','getpass','zipfile']
 for depend in dependencies:
     try:
         __import__(depend)
     except:
-        print('WARNING! %s not installed\nPlease run pip install %s'%(depend, depend))
+        print('WARNING! %s not installed\nPlease run pip install %s\n'%(depend, depend))
         task_summary += ['Please run pip install %s'%(depend)]
-print('Dependencies checked.')
+print('DONE! Dependencies checked.\n')
 
 #setup directory
 print('Lets set everything up')
+time.sleep(1)
 print('This is your working directory %s'%(os.getcwd()))
+time.sleep(1)
 answer = input('Do you want to install in this directory? (Y/N) ')
 if answer in ['n','N']:
     directory = input('Please give desired install directory: ')
@@ -42,41 +50,75 @@ else:
 if not os.path.isdir(directory):
     os.mkdir(directory)
 os.chdir(directory)
+print('DONE! Will install in %s\n'%(directory))
+time.sleep(2)
 
+install_type = input('What do you want to install?\n (1) KiteBase\n (2) KiteBase & KiteRange\n (3) KiteBase & KiteSwing\n (4) All the above\nResponse: ')
+while install_type not in ['1','2','3','4']:
+    install_type = input('Not a valid option\nWhat do you want to install?\n (1) KiteBase\n (2) KiteBase & KiteRange\n (3) KiteBase & KiteSwing\n (4) All the above\nResponse: ')
+
+
+filelist = {'1':['fno_calendar.pyc','Instance.pyc'],'2':['fno_calendar.pyc','Range.pyc','Instance.pyc'],'3':['fno_calendar.pyc','Swing.pyc','Container.pyc','Instance.pyc'],'4':['fno_calendar.pyc','Range.pyc','Instance.pyc','Swing.pyc','Container.pyc']}
 for file in os.listdir():
-        if file in ['fno_calendar.pyc','Range.pyc','Instance.pyc']:
+        if file in filelist[install_type]:
             os.remove(directory+bs+file)
 
-print('Fetching latest KiteRange...')
-url = 'https://hknnisar.github.io/download/KiteRange.zip'
+print('OKAY, Fetching latest modules...')
+time.sleep(1)
+url = 'https://hknnisar.github.io/download/KiteBase.zip'
 r = requests.get(url, allow_redirects=True)
-open('KiteRange.zip', 'wb').write(r.content)
-print('Proceeding installation in %s'%(os.getcwd()))
+open('KiteBase.zip', 'wb').write(r.content)
+if install_type == '2':
+    url = 'https://hknnisar.github.io/download/KiteRange.zip'
+    r = requests.get(url, allow_redirects=True)
+    open('KiteRange.zip', 'wb').write(r.content)
+elif install_type=='3':
+    url = 'https://hknnisar.github.io/download/KiteSwing.zip'
+    r = requests.get(url, allow_redirects=True)
+    open('KiteSwing.zip', 'wb').write(r.content)
+elif install_type=='4':
+    url = 'https://hknnisar.github.io/download/KiteRange.zip'
+    r = requests.get(url, allow_redirects=True)
+    open('KiteRange.zip', 'wb').write(r.content)
+    url = 'https://hknnisar.github.io/download/KiteSwing.zip'
+    r = requests.get(url, allow_redirects=True)
+    open('KiteSwing.zip', 'wb').write(r.content)
+
+print('DONE! Successfully fetched latest modules\n')
+time.sleep(1)
 
 # #copy modules to pycache
 # if not os.path.isdir(directory+'\\__pycache__'):
 #     os.mkdir(directory+'\\__pycache__')
 
+ziplist = {'1':['KiteBase.zip'],'2':['KiteBase.zip','KiteRange.zip'],'3':['KiteBase.zip','KiteSwing.zip'],'4':['KiteBase.zip','KiteRange.zip','KiteSwing.zip']}
+zipfilelist = {'KiteBase.zip':['Instance.py','fno_calendar.py'],'KiteRange.zip':['Range.py'],'KiteSwing.zip':['Swing.py','Container.py']}
 
 try:
-    with zipfile.ZipFile(directory+bs+'KiteRange.zip', 'r') as zip_ref:
-        zip_ref.extractall(directory,pwd=bytes(getpass.getpass('Please give secret code install KiteRange: '),'utf-8'))
-        files = ['Instance.py','fno_calendar.py','Range.py']
-        py_compile.main(files)
-        for file in files:
-            os.remove(directory+bs+file) 
-        pycache = '__pycache__'
-        for file in os.listdir(directory+bs+pycache):
-            os.rename(directory+bs+pycache+bs+file,directory+bs+file.split('.')[0]+'.pyc')
-        zip_ref.close()
-    os.remove(directory+bs+'KiteRange.zip')
+    for thefile in ziplist[install_type]:
+        with zipfile.ZipFile(directory+bs+thefile, 'r') as zip_ref:
+            zip_ref.extractall(directory,pwd=bytes(getpass.getpass('Please give secret code to install '+thefile[:-4]+':'),'utf-8'))
+            files = zipfilelist[thefile]
+            py_compile.main(files)
+            for file in files:
+                os.remove(directory+bs+file) 
+            pycache = '__pycache__'
+            for file in os.listdir(directory+bs+pycache):
+                os.rename(directory+bs+pycache+bs+file,directory+bs+file.split('.')[0]+'.pyc')
+            zip_ref.close()
+        os.remove(directory+bs+thefile)
+        
+    print('DONE! Modules successfully installed\n')
+    time.sleep(1)
 
     #chromedriver
     if not os.path.isfile(os.getcwd()+bs+'chromedriver.exe' if system == 'nt' else os.getcwd()+bs+'chromedriver'):
-        print('Please fetch a compatible chromedriver from https://chromedriver.chromium.org/downloads')
+        print('Please fetch a compatible chromedriver from https://chromedriver.chromium.org/downloads\n')
+        time.sleep(1)
         task_summary += ['Please fetch a compatible chromedriver from https://chromedriver.chromium.org/downloads']
     else:
-        print('Please ensure the chromedriver version is compatible with installed Chrome version')
+        print('Please ensure the chromedriver version is compatible with installed Chrome version\n')
+        time.sleep(1)
         task_summary += ['Please ensure the chromedriver version is compatible with installed Chrome version']
     
     #credentials
@@ -87,7 +129,8 @@ try:
     else:
         answer = 'n'
     if answer in ['N','n']:
-        print("Lets store and encrypt all credentials")
+        print("\nLets store and encrypt all credentials")
+        time.sleep(1)
         api_key = input('Please give the api_key: ')
         api_secret = input('Please give the api_secret: ')
         client_id = input('Please give the Kite Client ID: ')
@@ -103,22 +146,31 @@ try:
         
         os.system('openssl enc -in cred.dat -out cred -e -aes256 -k %s'%(getpass.getpass('Please set a password for encryption: ')))
         os.remove(directory+bs+'cred.dat')
-    task_summary += ['After importing Range, call Range.help() to get an introduction']
-    print('__'*40+'\nHere is a short introduction on how to use the Range module')
-    try:
-        _temp = __import__('Range',fromlist=['Range'])
-        getattr(_temp,'Range').help()
-    except:
-        print('Please install all dependencies')
+    # task_summary += ['After importing Range, call Range.help() to get an introduction']
+    # print('__'*40+'\nHere is a short introduction on how to use the Range module')
+    # try:
+    #     _temp = __import__('Range',fromlist=['Range'])
+    #     getattr(_temp,'Range').help()
+    # except:
+    #     print('Please install all dependencies')
     if len(task_summary) > 0:
-        print('__'*40+'\n\nPlease complete the following tasks before running')
+        print('\n'+'__'*40+'\nDONE! You are almost all set! Please complete the following tasks before running')
+        time.sleep(1)
         for item in task_summary:
             print('-> '+item)
+    else:
+        print('\n'+'__'*40+'\nDONE! You are all set!')
+        time.sleep(1)
 except Exception as e:
+    print('\nSetup FAILED\n')
+    time.sleep(1)
     print(str(e))
-    print('To get a secret code please contact me on Telegram or Twitter @harshnisar or email me at harsh@harshnisar.com')
+    time.sleep(1)
+    print('\nTo get a secret code please contact me on Telegram or Twitter @harshnisar or email me at harsh@harshnisar.com')
+    time.sleep(1)
     for file in os.listdir():
-        if file in ['fno_calendar.pyc','Range.pyc','Instance.pyc','fno_calendar.py','Range.py','Instance.py']:
+        if file in ['fno_calendar.pyc','Range.pyc','Instance.pyc','fno_calendar.py','Range.py','Instance.py','Swing.pyc','Container.pyc','Swing,py','Container.py']:
             os.remove(directory+bs+file)
-    if os.path.isfile(directory+bs+'KiteRange.zip'):
-        os.remove(directory+bs+'KiteRange.zip')
+    for thefile in ziplist[install_type]:
+        if os.path.isfile(directory+bs+thefile):
+            os.remove(directory+bs+thefile)
